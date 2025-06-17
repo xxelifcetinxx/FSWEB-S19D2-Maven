c896631366472d3e17aff72aef7e819e8f2d6d88
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-17 15:45:06 +0300

    address table done

---
+++ src/main/java/com/workintech/s18d4/entity/Address.java
@@ -0,0 +1,36 @@
+package com.workintech.s18d4.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "no")
+    private int no;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private Customer customer;
+}
