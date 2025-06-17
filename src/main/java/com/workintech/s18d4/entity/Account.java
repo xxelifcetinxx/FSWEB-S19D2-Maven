7d88efadeef66c082defae9a6ebc5f735e57a539
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-17 15:45:33 +0300

    account table done

---
+++ src/main/java/com/workintech/s18d4/entity/Account.java
@@ -0,0 +1,30 @@
+package com.workintech.s18d4.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="account", schema = "fsweb")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private long id;
+
+    @Column(name = "account_name")
+    private String accountName;
+
+    @Column(name = "money_amount")
+    private double moneyAmount;
+
+    @JsonBackReference
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
